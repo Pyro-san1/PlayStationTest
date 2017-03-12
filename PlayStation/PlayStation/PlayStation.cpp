028c1be40e016f6d4faf714c61785659dd83ed5b
Pyro-san <oprre.born@gmail.com>
2017-03-12 17:07:41 +0000

    [Add] curl

--- PlayStation/PlayStation/PlayStation.cpp
+++ PlayStation/PlayStation/PlayStation.cpp
@@ -5,8 +5,21 @@
 #include <iostream>
 #include <string>
 
-int main()
+#include "libCurl/include/curl/curl.h"
+
+int main(int argc, char *argv[])
 {
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    CURL *curl = curl_easy_init();
+    if (curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, "http://www.dynamsoft.com");
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    printf("Press any key to continue\n");
+    getchar();
     return 0;
 }
 
