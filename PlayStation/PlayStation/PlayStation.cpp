df5d9b988886d754a1d40b1e3ca5db377160b2c4
Pyro-san <oprre.born@gmail.com>
2017-03-12 22:55:55 +0000

    [Silence] warnings from libs [Add] includes, jsoncpp package info

--- PlayStation/PlayStation/PlayStation.cpp
+++ PlayStation/PlayStation/PlayStation.cpp
@@ -2,29 +2,60 @@
 //
 
 #include "stdafx.h"
+
 #include <iostream>
-#include <string>
-#include <cstdio>
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
+#include <filesystem>
+#include <vector>
 
 #include "SDL.h"
 #include "SDL_image.h"
 
-#include "curl/curl.h"
+#include "FileDownloader.h"
+#include "JsonParser.h"
+
+std::string Usage()
+{
+    std::string CRLF = "\r\n";
+    std::string usage;
+    usage += "---------------------------------------------------------";
+    usage += CRLF;
+    usage += "See proper parameterlist and order below:";
+    usage += CRLF;
+    usage += "  PlayStation.exe pngFilesInThisJson.json";
+    usage += CRLF;
+    usage += CRLF;
+    usage += "pngFilesInThisJson.json must contain at least one link or full path to a png file!:";
+    usage += CRLF;
+    usage += "---------------------------------------------------------";
+    usage += CRLF;
+
+    return usage;
+}
 
 int main(int argc, char *argv[])
 {
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    CURL *curl = curl_easy_init();
-    if (curl) {
-        CURLcode res;
-        curl_easy_setopt(curl, CURLOPT_URL, "http://www.dynamsoft.com");
-        res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
+    if (2 != argc)
+    {
+        std::cout << "Error: Not enough parameters." << std::endl;
+        std::cout << Usage() << std::endl;
+        return -1;
     }
-    curl_global_cleanup();
+
+    std::experimental::filesystem::path path = std::experimental::filesystem::current_path();
+    path += "\\";
+    path += "Download\\";
+    std::experimental::filesystem::create_directory(path);
+
+    std::string const jsonFileName = path.string() + "pngList.json";
+
+    FileDownloader downloader;
+    downloader.downloadFile(argv[1], jsonFileName);
+
+    JsonParser jsonParser;
+    jsonParser.parseJson(jsonFileName);
+
+    std::vector<std::string> downloadedFiles = downloader.downloadFiles(jsonParser.getPngUrls(), path.string());
+    unsigned int currentIndex = 0;
 
     SDL_Event event;
 
@@ -38,17 +69,21 @@ int main(int argc, char *argv[])
         "PictureViewer",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        320,
-        568,
+        100,
+        100,
         SDL_WINDOW_SHOWN
     );
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    background_surface = IMG_Load("c:/__cucc/interviews/PlayStation/a.png");
+    background_surface = IMG_Load(downloadedFiles[currentIndex].c_str());
     background_texture = SDL_CreateTextureFromSurface(renderer, background_surface);
 
     int w;
     int h;
+
+    SDL_QueryTexture(background_texture, NULL, NULL, &w, &h);
+    SDL_SetWindowSize(window, w, h);
+
 
     while (true)
     {
@@ -64,23 +99,37 @@ int main(int argc, char *argv[])
             {
                 case SDLK_RIGHT:
                 {
-                    std::cout << "rightPressed" << std::endl;
-                    background_surface = IMG_Load("c:/__cucc/interviews/PlayStation/b.png");
+                    if (currentIndex == static_cast<unsigned int>(downloadedFiles.size() - 1))
+                    {
+                        currentIndex = 0;
+                    }
+                    else
+                    {
+                        currentIndex++;
+                    }
+                    background_surface = IMG_Load(downloadedFiles[currentIndex].c_str());
                     background_texture = SDL_CreateTextureFromSurface(renderer, background_surface);
                     break;
                 }
                 case SDLK_LEFT:
                 {
+                    if (currentIndex == 0)
+                    {
+                        currentIndex = static_cast<unsigned int>(downloadedFiles.size() - 1);
+                    }
+                    else
+                    {
+                        currentIndex--;
+                    }
                     std::cout << "leftPressed" << std::endl;
-                    background_surface = IMG_Load("c:/__cucc/interviews/PlayStation/a.png");
+                    background_surface = IMG_Load(downloadedFiles[currentIndex].c_str());
                     background_texture = SDL_CreateTextureFromSurface(renderer, background_surface);
                     break;
                 }
             }
+            SDL_QueryTexture(background_texture, NULL, NULL, &w, &h);
+            SDL_SetWindowSize(window, w, h);
         }
-
-        SDL_QueryTexture(background_texture, NULL, NULL, &w, &h);
-        SDL_SetWindowSize(window, w, h);
 
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, background_texture, NULL, NULL);
@@ -92,75 +141,6 @@ int main(int argc, char *argv[])
     SDL_FreeSurface(background_surface);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
-}
-
-// Get a random number between 0 and 255
-int randCol()
-{
-    return rand() % 255 + 0;
-}
-
-/*
-int main(int argc, char** argv)
-{
-    // Initialize the random number generator
-    srand(time(NULL));
-
-    // Initialize SDL
-    SDL_Init(SDL_INIT_VIDEO);
-
-    // Open a 800x600 window and define an accelerated renderer
-    SDL_Window* window = SDL_CreateWindow("SDL2 & Code::Blocks", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-    // Initial renderer color
-    SDL_SetRenderDrawColor(renderer, randCol(), randCol(), randCol(), 255);
-
-    bool running = true;
-    Uint32 old_time = 0, change_color_time = 1000, new_time;
-    SDL_Event event;
-    while (running)
-    {
-        // Check for various events (keyboard, mouse, touch, close)
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_KEYDOWN)
-            {
-                const char* key = SDL_GetKeyName(event.key.keysym.sym);
-                if (strcmp(key, "Q") == 0 || strcmp(key, "Escape") == 0)
-                {
-                    running = false;
-                }
-            }
-            else if (event.type == SDL_QUIT)
-            {
-                running = false;
-            }
-        }
-
-        // Clear buffer
-        SDL_RenderClear(renderer);
-
-        // Draw here ...
-
-        // Switch buffers
-        SDL_RenderPresent(renderer);
-
-        new_time = SDL_GetTicks();
-        if (new_time - old_time > change_color_time)
-        {
-            SDL_SetRenderDrawColor(renderer, randCol(), randCol(), randCol(), 255);
-            old_time = new_time;
-        }
-    }
-
-    // Release any of the allocated resources
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 
     return 0;
 }
-*/
